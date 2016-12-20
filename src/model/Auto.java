82ed8034c7418dd1f308577108d9107f4cd3df5e
chaostype <s50586@beuth-hochschule.de>
2016-12-20 07:57:46 +0100

    Update Auto.java

--- src/model/Auto.java
+++ src/model/Auto.java
@@ -3,4 +3,42 @@ package model;
 public class Auto {
 
 
+	public int getPositionX() {
+		return this.positionX;
+	}
+	
+	public int getPositionY() {
+		return this.positionY;
+	}
+	
+	public int getOldPositionX() {
+		return this.oldPositionX;
+	}
+	
+	public int getOldPositionY() {
+		return this.oldPositionY;
+	}
+
+
+	public void setPositionX(int positionX) {
+		this.positionX = positionX;
+	}
+	
+	public void setPositionY(int positionY) {
+		this.positionY = positionY;
+	}
+	
+	public void setOldPositionX(int oldPositionX) {
+		this.oldPositionX = oldPositionX;
+	}
+	
+	public void setOldPositionY(int oldPositionY) {
+		this.oldPositionY = oldPositionY;
+	}
+	
+	public void getImage() {
+		return this.frogPic;
+	}  
+  
+  
 }
