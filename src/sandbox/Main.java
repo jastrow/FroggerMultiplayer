2f86e2f9f4d996881b809cc0422e917e7ef83ceb
Salim Oussayfi <salimoussayfi@Salim-Oussayfi.local>
2016-12-22 17:32:24 +0100

    change images

--- src/sandbox/Main.java
+++ src/sandbox/Main.java
@@ -5,29 +5,52 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+	
+	//default image
+	String imageRessoure = "Frosch_Animation_hochRunter_Bewegung_01.png";	
+	ImageView image = new ImageView(new Image(getClass().getResourceAsStream(imageRessoure)));
+	
+	
     @Override
     public void start(Stage primaryStage) throws Exception {
-        HBox pane = new HBox();
-        //Image image = new Image("Frosch_Animation_hochRunter_Bewegung_01.png");
+        HBox root = new HBox();
+        
+        
         Button button = new Button("Click some arrow");
         Label label = new Label("Click some arrow");
-        pane.getChildren().add(button);
-        pane.getChildren().add(label);
+        root.getChildren().add(button);
+        root.getChildren().add(label);
+        root.getChildren().add(image);
         
-        Scene scene = new Scene(pane, 200, 200);
+        Scene scene = new Scene(root, 200, 200);
         
         primaryStage.setScene(scene);
         primaryStage.show();
 
-        pane.setOnKeyPressed(new EventHandler<KeyEvent>() {
-
+        root.setOnKeyPressed(new EventHandler<KeyEvent>() {
+        	
+        	/**
+        	 * Method changes displayed image
+        	 * @param newImage
+        	 */
+        	public void changeImage(String newImage){
+        		root.getChildren().remove(image);
+        		image.setImage(null);
+                imageRessoure = newImage;
+                ImageView image = new ImageView(new Image(getClass().getResourceAsStream(newImage)));
+                root.getChildren().add(image);
+                primaryStage.show();
+        	}
+        	
+        	
             @Override
             public void handle(KeyEvent event) {
             	if (event.getCode() == KeyCode.ENTER) {
@@ -35,15 +58,19 @@ public class Main extends Application {
                 }
             	if (event.getCode() == KeyCode.UP) {
                     System.out.println("Up Pressed");
+                    changeImage("Frosch_Animation_runterHoch_Bewegung_01.png");
                 }
             	if (event.getCode() == KeyCode.DOWN) {
                     System.out.println("Down Pressed");
+                    changeImage("Frosch_Animation_hochRunter_Bewegung_01.png");
                 }
             	if (event.getCode() == KeyCode.LEFT) {
                     System.out.println("Left Pressed");
+                    changeImage("Frosch_Animation_rechtsLinks_Bewegung_01.png");
                 }
             	if (event.getCode() == KeyCode.RIGHT) {
                     System.out.println("Right Pressed");
+                    changeImage("Frosch_Animation_linksRechts_Bewegung_01.png");
                 }
             }
         });
