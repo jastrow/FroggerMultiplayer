7c8d73c2f1685c10afdb574964619d82a6af4872
Mike Jastrow <mj@mdnetz.de>
2017-01-10 20:22:04 +0100

    gitignore .classpath added

--- src/model/HighScoreLocal.java
+++ src/model/HighScoreLocal.java
@@ -6,6 +6,15 @@ public class HighScoreLocal {
 	private String date;
 	private Integer time;
 	
+	public HighScoreLocal(String name, String date, Integer time) {
+		this.name = name;
+		this.date = date;
+		this.time = time;
+	}
+	
+	public String getName() { return this.name; }
+	public String getDate() { return this.date; }
+	public Integer getTime() { return this.time; }
 	
 	@Override
 	public String toString() {
